7fe2ed7e7d239434d496ea59d509150743cfcd71
03Jacobbattenfield <72627563+03Jacobbattenfield@users.noreply.github.com>
2020-10-28 14:07:15 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function ruin(){
+    document.getElementById("container").style.display="none";
+    document.getElementById("hidtxt").style.color="white";
+    document.getElementById("hidbut").style.backgroundColor="white";
+    document.getElementById("hidbut").style.border="1px solid white";
+}
+function no(){
+    document.getElementById("hidtxt").innerHTML="this was on you.";
+    document.getElementById("hidbut").style.display="none";
+}
